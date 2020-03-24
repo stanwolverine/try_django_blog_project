3115ecfe8a1cd828b673c41b5d1caa78569d26a5
Jatin Nagar <jatin.nagar@healthmug.com>
2020-03-24 21:49:36 +0530

    static and media files phase 2

--- try_django/settings.py
+++ try_django/settings.py
@@ -125,4 +125,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+LOCAL_STATIC_CDN_PATH = os.path.join(
+    os.path.dirname(BASE_DIR), 'static_cdn_test')
+
+STATIC_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH,
+                           'static')  # live cdn like - AWS S3
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+
+MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media')
+MEDIA_URL = '/media/'
--- try_django/urls.py
+++ try_django/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include  # re_path and url
 from blog.views import blog_post_create_view
@@ -39,3 +40,11 @@ urlpatterns = [
     # > Above will render for both 'page' and 'pages' url string
     # '''
 ]
+
+if settings.DEBUG:
+    # test mode
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
